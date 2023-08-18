04fdc7099df78ad4304db3b02975d55a64e5b617
mrvigneshwaranbe <139253412+mrvigneshwaranbe@users.noreply.github.com>
2023-08-18 17:53:40 +0530

    Add files via upload

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import ThemeContext from "../../context/themeContext";
+import heroImg from "../../assets/phone.svg";
+import "./hero.css";
+
+const Hero = () => {
+  const { theme } = useContext(ThemeContext);
+  return (
+    <section className="hero" data-theme={theme}>
+      <div className="container --grid-15">
+        <div className="hero-text">
+          <h1>Digital Shop, Physically Located in all over Tamil Nadu!</h1>
+          <p>
+            It takes real to know real. An we believe everyone is real. At our
+            store you can be sure to find authentic products to meet your daily
+            as well as luxurious need. Looking forward to seeing you today.
+            Cheers!
+          </p>
+          <div className="--flex-start">
+            <button className="--btn btn-video">Watch Video</button>
+            <button className="--btn --btn-danger">Sign Up</button>
+          </div>
+        </div>
+        <div className="--text-center">
+          <img src={heroImg} alt="hero" width={180} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
